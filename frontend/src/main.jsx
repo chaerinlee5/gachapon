eb441de33e952fd0d064620539d09cdb6294a8d4
Chaerin Lee <chaerinlee1215@gmail.com>
2025-09-28 02:44:09 -0400

    fixing main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,13 @@
-import { HashRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // or HashRouter if you use that
 import AppWrapper from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <AppWrapper />
-  </HashRouter>
+  <React.StrictMode>
+    <BrowserRouter basename="/gachapon"> 
+      <AppWrapper />
+    </BrowserRouter>
+  </React.StrictMode>
 );
